4c1eab817c5d682b8cbf654e1e5e5c945e40889d
Frovu <sfrovis@gmail.com>
2021-09-09 20:34:13 +0300

    fix: data fetch progress

--- static/js/applications/temperature.js
+++ static/js/applications/temperature.js
@@ -1,31 +1,39 @@
 async function query() {
-	return await fetch('api/temp/?from=1630067000&to=1641199298&lat=55.47&lon=37.32', { method: 'GET' });
+	return await fetch('api/temp/?from=1580504400&to=1641199298&lat=55.47&lon=37.32', { method: 'GET' });
 }
 
 async function fetchData() {
 	const progress = document.getElementById('progress');
-	for (let loop = true; loop;) {
-		const resp = await query().catch(()=>{});
-		if (resp && resp.status === 200) {
-			const body = await resp.json().catch(()=>{});
-			console.log(body)
-			if (body.status === 'ok') {
-				console.log('done');
-				break;
-			} else if (body.status === 'busy') {
-				progress.innerHTML = body.download ? `Downloading: ${body.download}` : 'Calculating';
-			} else if (body.status === 'unknown') {
-				// TODO:
-			}
-		} else {
-			console.log('request failed', resp && resp.status);
+	const resp = await query().catch(()=>{});
+	if (resp && resp.status === 200) {
+		const body = await resp.json().catch(()=>{});
+		if (body.status === 'ok') {
+			progress.innerHTML = 'done!';
+			return true;
+		} else if (body.status === 'busy') {
+			progress.innerHTML = body.download ? `Downloading: ${(100*body.download).toFixed(0)} %` : 'Calculating';
+		} else if (body.status === 'unknown') {
+			// TODO:
 		}
+	} else {
+		console.log('request failed', resp && resp.status);
 	}
+}
 
+function startFetch() {
+	fetchData().then(ok => {
+		if (!ok) {
+			const interval = setInterval(() => {
+				fetchData().then(okk => {
+					if (okk) clearInterval(interval);
+				});
+			}, 1000);
+		}
+	});
 }
 
 const btn = document.getElementById('button');
-btn.onclick = fetchData;
+btn.onclick = startFetch;
 
 export function ping() {
 	// console.log('temperature');
