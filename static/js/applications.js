cdcec6c5396079c087c2a86f8cebe4d20d9ddc32
Frovu <sfrovis@gmail.com>
2021-10-01 18:55:55 +0300

    fix: tabs cache for app switching

--- static/js/applications.js
+++ static/js/applications.js
@@ -9,7 +9,7 @@ const applications = {
 const tabsCache = {};
 
 let active;
-let select;
+const selects = [];
 
 export function swithApp(nextApp) {
 	const tabs = document.getElementsByClassName('tab');
@@ -18,27 +18,33 @@ export function swithApp(nextApp) {
 		if (!tabsCache[active]) tabsCache[active] = {};
 		if (app.unload) app.unload();
 		for (const tab of tabs) {
-			tabsCache[active][tab.id] = tab.cloneNode(true);
+			tabsCache[active][tab.id] = tab;
+			const newTab = tab.cloneNode(true);
+			newTab.innerHTML = '';
+			document.body.replaceChild(newTab, tab);
 		}
 	}
 	if (tabsCache[nextApp]) { // restore app's tabs from cache
 		for (const tab of tabs) {
+			// console.log(tabsCache[nextApp][tab.id])
 			document.body.replaceChild(tabsCache[nextApp][tab.id], tab);
 		}
 	} else {
 		const apptab = document.getElementById('app-tab');
 		apptab.innerHTML = '<label for="app">Select application:</label>';
-		if (!select) {
-			select = document.createElement('select');
-			for (const app in applications) {
-				const opt = document.createElement('option');
-				opt.value = app;
-				opt.innerHTML = app.charAt(0).toUpperCase() + app.slice(1);
-				if (app === nextApp) opt.selected = 'selected';
-				select.append(opt);
-			}
-			select.onchange = () => swithApp(select.value);
+		const select = document.createElement('select');
+		for (const app in applications) {
+			const opt = document.createElement('option');
+			opt.value = app;
+			opt.innerHTML = app.charAt(0).toUpperCase() + app.slice(1);
+			if (app === nextApp) opt.selected = 'selected';
+			select.append(opt);
 		}
+		select.onchange = () => {
+			swithApp(select.value);
+			selects.forEach(sel => sel.value = select.value);
+		};
+		selects.push(select);
 		apptab.append(select);
 		applications[nextApp].initTabs();
 	}
