7ace0b736e39c4b55aa6ad83cd40cb054455b980
Frovu <sfrovis@gmail.com>
2021-12-06 17:45:18 +0300

    fix: gfs tries optimization

--- data_source/temperature_model/gfs_parser.py
+++ data_source/temperature_model/gfs_parser.py
@@ -18,24 +18,23 @@ def _download(filename, latlon, fcst_date, fcst_hour, source='gfs'):
     query += f'file={source}.t{hh}z.pgrb2.0p25.f{fcst_hour:03}'
     query += f'&dir=%2F{source}.{yyyymmdd}%2F{hh}%2Fatmos'
     query += f'&subregion=&leftlon={latlon[1][0]}&rightlon={latlon[1][1]}&bottomlat={latlon[0][0]}&toplat={latlon[0][1]}'
-    logging.warning(f'Query GFS {source}.{yyyymmdd}/{hh}+{fcst_hour:03}')
+    logging.debug(f'Query GFS {source}.{yyyymmdd}/{hh}+{fcst_hour:03}')
     res = requests.get(f'{_GFS_URL}{query}{_GFS_QUERY_VARS}', stream=True)
     if res.status_code == 200:
         with open(filename, 'wb') as f:
             for chunk in res.iter_content(1024):
                 f.write(chunk)
         return filename
+    logging.warning(f'Failed GFS [{res.status_code}] {source}.{yyyymmdd}/{hh}+{fcst_hour:03}')
     return False
 
 def _tries_list(dtime, depth=8, PER_H=6):
-    tries = []
+    tries, now = [], datetime.utcnow()
     fcst_date = dtime.replace(hour = dtime.hour // PER_H * PER_H)
     fcst_hour = dtime.hour % PER_H
-    gdas_ready = datetime.now() - timedelta(hours=4)
-    gfs_ready = datetime.now()
-    if fcst_date + timedelta(hours=8) < datetime.now(): # gdas computes ~ 7 hours
+    if fcst_date + timedelta(hours=8) < now: # gdas computes ~ 7 hours
         tries.append((fcst_date, fcst_hour, 'gdas'))
-    while fcst_date + timedelta(hours=4) > datetime.now(): # rewind to until gfs ready
+    while fcst_date + timedelta(hours=4) > now: # rewind to until gfs ready
         fcst_date -= timedelta(hours=PER_H)
         fcst_hour += PER_H
     for i in range(depth):
@@ -64,21 +63,20 @@ def _calc_one_hour(timestamp, lat, lon, grid_margin=2):
                     lvl = ndimage.map_coordinates(grb.values, ([lat_i], [lon_i]), mode='nearest')
                     result[LEVELS.index(grb.level)] = lvl
                 break
-            else:
-                logging.warning(f'Can not get GFS, fcst_h={args[1]}')
     except:
         logging.error(f'Failed to get GFS data for {dtime}')
     if os.path.exists(fname): os.remove(fname)
     return result
 
+# returns 17-levels temp for coordinates, time range with 1h period edge included
 def obtain(lat, lon, t_from, t_to, PERIOD=3600):
     times = [t for t in range(t_from, t_to + 1, PERIOD)]
     with ThreadPoolExecutor(max_workers=32) as e:
-        result = list(e.map(lambda t: _calc_one_hour(t, lat, lon), times))
-    print(result)
+        result = np.array(list(e.map(lambda t: _calc_one_hour(t, lat, lon), times)))
+    return result
 
 lat = 55.47
 lon = 37.32
 fr = 1638662400#- 3600 * 24 * 4
-to = fr + 48*3600
-obtain(lat, lon, fr, to)
+to = fr + 2*3600
+print(obtain(lat, lon, fr, to))
