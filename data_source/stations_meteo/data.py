717aac681b4130acc0b1ae4bd5c4ed492990c643
Frovu <sfrovis@gmail.com>
2022-01-16 21:19:37 +0300

    fix: local meteo time trim

--- data_source/stations_meteo/data.py
+++ data_source/stations_meteo/data.py
@@ -2,8 +2,9 @@ import data_source.temperature_model.temperature as temperature
 import data_source.stations_meteo.db_proxy as proxy
 import data_source.stations_meteo.parser as parser
 from core.sequence_filler import SequenceFiller, fill_fn
-from datetime import datetime
+from datetime import datetime, timezone
 from math import floor, ceil
+import logging as log
 
 scheduler = SequenceFiller(ttl=15)
 completed_query_chache = dict()
@@ -14,7 +15,10 @@ def get_with_model(lat, lon, t_from, t_to, period=PERIOD):
     lon = round(float(lon), 2)
     token = (lat, lon)
     t_from, t_to = period * floor(t_from / period), period * ceil(t_to / period)
-    is_done, info = scheduler.status((token, t_from, t_to))
+    key = (token, t_from, t_to)
+    t_trim = datetime.utcnow().replace(tzinfo=timezone.utc).timestamp() // period * period - period
+    if t_trim > t_to: t_trim = t_to
+    is_done, info = scheduler.status(key)
     if is_done == False:
         return 'failed' if info.get('failed') else 'busy', info
     station = proxy.select_station(lat, lon)
@@ -23,10 +27,12 @@ def get_with_model(lat, lon, t_from, t_to, period=PERIOD):
     model_status, model_r = temperature.get(lat, lon, t_from, t_to, True)
     if model_status ==  'unknown':
         return model_status, model_r
-    if model_status == 'ok' and not proxy.analyze_integrity(station, t_from, t_to):
+    if model_status == 'ok' and not proxy.analyze_integrity(station, t_from, t_trim):
         return 'ok', proxy.select(station, t_from, t_to, True)
-    q = scheduler.do_fill(token, t_from, t_to, period, parser.get_tasks(station, period, fill_fn))
+    log.info(f'LOCAL METEO: Filling {station} {t_from}:{t_trim}')
+    q = scheduler.do_fill(token, t_from, t_trim, period,
+        parser.get_tasks(station, period, fill_fn), key_overwrite=key)
     if model_status == 'accepted':
         model_r.append_tasks(q.tasks) # use temp_model query object
-        scheduler.query((token, t_from, t_to), model_r)
+        scheduler.query(key, model_r)
     return 'accepted', None
