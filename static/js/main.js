64678dcd57c3cd73a22d115512bafac942144c1c
Frovu <sfrovis@gmail.com>
2022-02-10 22:40:33 +0300

    add: register ui

--- static/js/main.js
+++ static/js/main.js
@@ -12,12 +12,15 @@ const tabs = [
 let activeTab = 'app';
 
 async function login() {
+	const pwdShow = document.getElementById('repeat-input');
+	pwdShow.hidden = 'true';
 	const loginInp = document.getElementById('uname');
 	const pwdInp = document.getElementById('pwd');
 	const uname = loginInp.value;
 	const password = pwdInp.value;
 	const error = document.getElementById('login-error');
 	const res = await fetch('api/auth/login', {
+		headers: {'Content-Type': 'application/x-www-form-urlencoded'},
 		method: 'POST',
 		body: `login=${uname}&password=${password}`
 	});
@@ -34,17 +37,68 @@ async function login() {
 	} else if (res.status === 404) {
 		loginInp.classList.add('invalid');
 		error.innerHTML = 'User not found.';
-	} else if (res.status === 401) {
+	} else if (res.status === 401 || !password.length) {
 		pwdInp.classList.add('invalid');
 		error.innerHTML = 'Wrong password.';
 	} else {
 		error.innerHTML = 'Failed to login.';
-		console.log(res);
+		console.error(res);
+	}
+}
+
+async function logout() {
+	const res = await fetch('api/auth/logout');
+	if (res.status === 200) {
+		const elIn = document.getElementById('logged-in');
+		const elOut = document.getElementById('logged-out');
+		const unameEl = document.getElementById('username');
+		unameEl.innerHTML = '';
+		elOut.hidden = null;
+		elIn.hidden = 'true';
 	}
 }
 
 async function register() {
-
+	const loginInp = document.getElementById('uname');
+	const pwdInp = document.getElementById('pwd');
+	const pwdRepeat = document.getElementById('pwd-repeat');
+	const error = document.getElementById('login-error');
+	loginInp.classList.remove('invalid');
+	pwdInp.classList.remove('invalid');
+	pwdRepeat.classList.remove('invalid');
+	const pwdShow = document.getElementById('repeat-input');
+	pwdShow.hidden = null;
+	const uname = loginInp.value;
+	const password = pwdInp.value;
+	if (!uname.includes('@')) {
+		loginInp.classList.add('invalid');
+		error.innerHTML = 'Doesn\t look like an email.';
+		return;
+	}
+	if (password.length < 6) {
+		pwdInp.classList.add('invalid');
+		error.innerHTML = 'Password too short.';
+		return;
+	}
+	if (password !== pwdRepeat.value) {
+		pwdRepeat.classList.add('invalid');
+		error.innerHTML = 'Passwords do not match.';
+		return;
+	}
+	const res = await fetch('api/auth', {
+		headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+		method: 'POST',
+		body: `login=${uname}&password=${password}`
+	});
+	if (res.status === 200) {
+		await login();
+	} else if (res.status === 409) {
+		loginInp.classList.add('invalid');
+		error.innerHTML = 'User already exists.';
+	} else {
+		error.innerHTML = 'Failed to register.';
+		console.error(res);
+	}
 }
 
 async function checkLogin() {
@@ -89,12 +143,11 @@ window.onload = () => {
 		applications.init();
 	};
 
-	const loggedIn = checkLogin();
+	checkLogin();
 	applications.updateView();
-	const loginBtn = document.getElementById('login');
-	const regtrBtn = document.getElementById('register');
-	loginBtn.onclick = login;
-	regtrBtn.onclick = register;
+	document.getElementById('login').onclick = login;
+	document.getElementById('logout').onclick = logout;
+	document.getElementById('register').onclick = register;
 
 	for (const tab of tabs) {
 		const el = document.getElementById(`${tab}-btn`);
