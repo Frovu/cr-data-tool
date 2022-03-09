a570ca07be35983b9caf32b38e8b0e8ef98be678
Frovu <sfrovis@gmail.com>
2022-03-09 17:53:23 +0300

    add: parse gsm result

---
+++ scripts/parse_gsm_result.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+import os, psycopg2, psycopg2.extras
+pg_conn = psycopg2.connect(
+    dbname = os.environ.get("MUON_DB_NAME") or "cr_cr",
+    user = os.environ.get("MUON_DB_USER") or "crdt",
+    password = os.environ.get("MUON_DB_PASS"),
+    host = os.environ.get("MUON_DB_HOST") )
+
+PATH = 'tmp/GSM.dat'
+
+def parse():
+    print(f'Reading file: {PATH}')
+    with open(PATH) as file:
+        for line in file:
+            if '-'*64 in line:
+                break
+        data = []
+        for line in file:
+            split = line.split()
+            time = datetime(*[int(d) for d in split[0].split('.')], int(split[1])-1)
+            data.append((time, *[float(v) for v in split[3:]]))
+    print(f'Parsed [{len(data)}] from {data[0][0]} to {data[-1][0]}')
+    print('Inserting...', end='')
+    with pg_conn.cursor() as cursor:
+        query = 'INSERT INTO gsm_result VALUES %s ON CONFLICT(time) DO NOTHING'
+        psycopg2.extras.execute_values(cursor, query, data)
+        pg_conn.commit()
+    print('done!')
+
+if __name__ == '__main__':
+    parse()
