c2643a14d6db72eaa9af6b5d1c0b58615db763bf
Frovu <sfrovis@gmail.com>
2022-06-02 13:10:46 +0300

    add: download gflux

--- data_source/atmosphere/download.py
+++ data_source/atmosphere/download.py
@@ -4,27 +4,26 @@ import logging as log
 import os
 
 class Downloader(Scheduler):
-    def __init__(self, ttl=0):
-        super().__init__(ttl=ttl)
+    def __init__(self, path, filename):
+        super().__init__(ttl=0)
+        self.filename = filename
+        self.path = path
+
+    def _download_noaa(self, progress, year):
+        fname = self.filename(year)
+        ftp = FTP('ftp2.psl.noaa.gov')
+        log.debug('FTP login: '+ftp.login())
+        log.info(f'Downloading file: {fname}')
+        ftp.cwd(f'Datasets/ncep.reanalysis/{self.path}')
+        progress[1] += ftp.size(fname)
+        with open(os.path.join('tmp/ncep', fname), 'wb') as file:
+            def write(data):
+               file.write(data)
+               progress[0] += len(data)
+            ftp.retrbinary(f'RETR {fname}', write)
+        log.info(f'Downloaded file: {fname}')
 
     def download(self, year):
         if eq := self.get(year):
             return eq
-        return self.query_tasks(year, [(_download_model, (year,), f'downloading {year}.nc', True)])
-
-def filename(year):
-    return f'air.{year}.nc'
-
-def _download_model(progress, year):
-    fname = filename(year)
-    ftp = FTP('ftp2.psl.noaa.gov')
-    log.debug('FTP login: '+ftp.login())
-    log.info(f'Downloading file: {fname}')
-    ftp.cwd('Datasets/ncep.reanalysis/pressure')
-    progress[1] += ftp.size(fname)
-    with open(os.path.join('tmp/ncep', fname), 'wb') as file:
-        def write(data):
-           file.write(data)
-           progress[0] += len(data)
-        ftp.retrbinary(f'RETR {fname}', write)
-    log.info(f'Downloaded file: {fname}')
+        return self.query_tasks(year, [(self._download_noaa, (year,), f'downloading {year}.nc', True)])
--- data_source/atmosphere/ground_flux.py
+++ data_source/atmosphere/ground_flux.py
@@ -1,5 +1,11 @@
 import data_source.atmosphere.temperature as temp
 import data_source.atmosphere.parser as parser
-from core.util
 
-def
+def _fill(t_from, t_to):
+    pass
+
+def _bound(t_from, t_to):
+    pass
+
+def get(t_from, t_to):
+    pass
