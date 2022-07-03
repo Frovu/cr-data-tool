e3e9dbbbbdce55b349ce85785bc3260fcc425d30
Frovu <sfrovis@gmail.com>
2022-07-03 12:07:50 +0300

    add: prec idx4 sin

--- scripts/exp/precursor_index.py
+++ scripts/exp/precursor_index.py
@@ -47,30 +47,66 @@ def idx_polyfit(time, variations, directions, window: int = 3):
         result[i] = (idx, divergency, angle, residuals)
     return result
 
+def idx_sinr(time, variations, directions, window: int = 3, min_scl = 0.45):
+    sorted = np.argsort(directions)
+    variations, directions = variations[:,sorted], directions[sorted]
+    result = np.full((len(time), 4), np.nan, dtype=np.float32)
+    def fn(x, a, scale, sx, sy):
+        return np.cos(x * a * np.pi / 180 + sx) * scale + sy
+    for i in range(window, len(time)):
+        x = np.concatenate([directions + time[i-t] * 360 / 86400 for t in range(window)]) % 360
+        y = np.concatenate([variations[i-t] for t in range(window)])
+        filter = np.isfinite(y)
+        x, y = x[filter], y[filter]
+        amax, amin = x[np.argmax(y)], x[np.argmin(y)]
+        approx_dist = np.abs(amax - amin)
+        center_target = 180 if approx_dist < 180 else 360
+        shift = center_target - (amax + amin) / 2
+        x = (x + shift + 360) % 360
+        # bb = ([1, -np.inf, -np.inf, -np.inf], [2, np.inf, np.inf, np.inf])
+        bounds = (approx_dist if approx_dist > 180 else (360-approx_dist)) / 5
+        trim = np.where((x > bounds) & (x < 360-bounds))
+        try:
+            popt, pcov = optimize.curve_fit(fn, x[trim], y[trim])
+
+            # dist = np.array([fn(x[i], *popt)-y[i] for i in range(len(x))])
+            # print(np.mean(np.abs(dist)**2), np.var(y))
+            angle, scale = abs(popt[0]), abs(popt[1])
+            if angle < 1 or angle > 4: angle = 0
+            idx = (scale ** 2 * angle) if scale >= min_scl else 0
+            result[i] = (idx, scale, angle, abs(np.max(y) - np.min(y)))
+        except:
+            result[i] = (np.nan, np.nan, np.nan, np.nan)
+    return result
+
+
 def plot():
     fig, ((axt1, ax1, ax2), (axt2, ax3, ax4)) = plt.subplots(2, 3, gridspec_kw={'width_ratios': [2, 1, 1], 'height_ratios': [1, 1]})
     colors = ['#00FFAA', '#00AAFF', '#ccFF00', '#50FF00']
 
-    interval = [ datetime(2011, 2, 15), datetime(2011, 2, 21) ]
-    interval = [ datetime(2022, 5, 10), datetime(2022, 5, 20) ]
-    # interval = [ datetime(2021, 7, 4), datetime(2021, 7, 12) ]
+    # interval = [ datetime(2011, 2, 13), datetime(2011, 2, 18) ]
+    interval = [ datetime(2022, 5, 15), datetime(2022, 5, 22) ]
+    # interval = [ datetime(2021, 10, 31, 12), datetime(2021, 11, 4, 10) ]
+    # interval = [ datetime(2021, 6, 10), datetime(2021, 6, 14) ]
     interval = [ t.replace(tzinfo=timezone.utc).timestamp() for t in interval ]
     time, variation, directions = _get(*interval)
+    dtime = np.array([datetime.utcfromtimestamp(t) for t in time])
 
     # p1_res = np.array([index_1(time[i], variation[i], directions) for i in range(0, len(time))])
-    p1_res = index_windowed(time, variation, directions, 6)
-    p1_res_w2 = index_windowed(time, variation, directions, 5)
+    p1_res = idx_sinr(time, variation, directions, 3)
+    # p1_res_w2 = idx_polyfit(time, variation, directions, 5)
+    p1_res_w2 = idx_sinr(time, variation, directions, 3, 0.6)
     p1_idx, p1_div, p1_ang = p1_res[:,0], p1_res[:,1], p1_res[:,2]
     # print(p1_idx)
-    axt1.plot(time[~np.isnan(p1_idx)], p1_idx[~np.isnan(p1_idx)], color='#ff10a0', label=f'precursor_idx')
-    axt1.plot(time[~np.isnan(p1_res_w2[:,0])], p1_res_w2[:,0][~np.isnan(p1_res_w2[:,0])], color='#af10f0', label=f'precursor_idx')
     twx = axt1.twinx()
-    twx.plot(time, variation, color='#00ffff')
+    twx.plot(dtime, variation, lw=1, color='#00ffff')
+    axt1.plot(dtime[~np.isnan(p1_idx)], p1_idx[~np.isnan(p1_idx)], lw=3, color='#ff10a0', label=f'precursor_idx')
+    axt1.plot(dtime[~np.isnan(p1_res_w2[:,0])], p1_res_w2[:,0][~np.isnan(p1_res_w2[:,0])], color='#af10f0', label=f'precursor_idx')
 
-    axt2.plot(time, p1_res[:,3], lw=2, color='#ff10a0', label=f'resid')
-    axt2.plot(time, p1_div, lw=1.5, color='#00ffff', label=f'diff')
+    # axt2.plot(dtime, p1_res[:,3], lw=2, color='#ff10a0', label=f'resid')
+    axt2.plot(dtime[p1_ang>0], p1_div[p1_ang>0], lw=1.5, color='#00ffff', label=f'diff')
     twx = axt2.twinx()
-    twx.plot(time, p1_ang, lw=2, color=colors[2], label=f'angle')
+    twx.plot(dtime[p1_ang>0], p1_ang[p1_ang>0], lw=2, color=colors[2], label=f'a')
 
     # def plot_savgol(i, ax):
     #     nonlocal time, variation, directions
@@ -93,45 +129,45 @@ def plot():
         center_target = 180 if approx_dist < 180 else 360
         shift = center_target - (amax + amin) / 2
         x = (x + shift + 360) % 360
-        curve = np.poly1d(np.polyfit(x, y, 3))
-        roots = curve.deriv().roots
 
-        # rotated = np.concatenate((rotated[sorted], rotated[sorted][:len(sorted)//3]+360))
-        # variations = np.concatenate((variations, variations[:len(sorted)//3]))
         ax.plot(x, y, 'ro', ms=6)
-        # ax.plot((x+180)%360, y, 'co', ms=6)
-        # print(rotated)
-        # print(variations)
-        # # print(spline.derivative().roots())
-        # # circle = spline(np.arange(0, 360, 1))
         rng = np.arange(30, 330, 1)
-        angle = abs(roots[0] - roots[1]) % 360
-        if angle > 180: angle = 360 - angle
-        print('=', angle, '=', roots)
-        ax.plot(rng, curve(rng), 'b', lw=3)
+        # # circle = spline(np.arange(0, 360, 1))
+        # curve = np.poly1d(np.polyfit(x, y, 3))
+        # roots = curve.deriv().roots
+        # angle = abs(roots[0] - roots[1]) % 360
+        # if angle > 180: angle = 360 - angle
+        # print('=', angle, '=', roots)
+        # ax.plot(rng, curve(rng), 'b', lw=3)
         # x, y = signal.savgol_filter((rotated, variations), 181, 3)
         # ax.plot(x, y, 'y', lw=3)
         def fn(x, a, scale, sx, sy):
             return np.cos(x * a * np.pi / 180 + sx) * scale + sy
         bb = ([-np.inf, -np.inf, -np.inf, -np.inf], [np.inf, np.inf, np.inf, np.inf])
-        bounds = (approx_dist if approx_dist > 180 else (360-approx_dist)) / 4
+        bounds = (approx_dist if approx_dist > 180 else (360-approx_dist)) / 5
         print(round(bounds), round(approx_dist), round(shift))
         trim = np.where((x > bounds) & (x < 360-bounds))
-        popt, pcov = optimize.curve_fit(fn, x[trim], y[trim], bounds=bb)
-        print('\\_/', popt)
-        ax.plot(rng, fn(rng, *popt), 'c', lw=3)
+        try:
+            popt, pcov = optimize.curve_fit(fn, x[trim], y[trim], bounds=bb)
 
-        trim = np.where((x > 45) & (x < 360-45))
-        popt, pcov = optimize.curve_fit(fn, x[trim], y[trim], bounds=bb)
-        print('\\_/', popt)
-        ax.plot(rng, fn(rng, *popt), 'y', lw=3)
-        ax.set_title(f'i={np.round(p1_idx[i])} a={round(angle,1)}  {np.round(roots, 1)}')
+            dist = np.array([fn(x[trim][j], *popt)-y[trim][j] for j in range(len(trim[0]))])
+            print(np.mean(np.abs(dist)), np.var(y))
+            print('\\_/', popt)
+            ax.plot(rng, fn(rng, *popt), 'c', lw=3)
+
+            # trim = np.where((x > 30) & (x < 360-30))
+            # popt, pcov = optimize.curve_fit(fn, x[trim], y[trim], bounds=bb)
+            # print('\\_/', popt)
+            # ax.plot(rng, fn(rng, *popt), 'y', lw=3)
+            ax.set_title(f'{dtime[i]} i={p1_idx[i]:.2f} a={p1_ang[i]:.2f} s={p1_div[i]:.2f}')
+        except:
+            print('failed')
 
     half = len(p1_idx)//2
-    plot_idx(np.nanargmax(p1_idx)+1, ax1)
-    plot_idx(np.nanargmin(p1_ang)+1, ax2)
-    plot_idx(np.nanargmax(p1_idx[:half])+half+1, ax3)
-    plot_idx(np.nanargmin(p1_ang)-2, ax4)
+    plot_idx(np.nanargmax(p1_idx[:half]), ax1)
+    plot_idx(np.nanargmax(p1_idx[half//2:half])+half//2, ax2)
+    plot_idx(np.nanargmax(p1_res[:,3][:half]), ax3)
+    plot_idx(np.nanargmax(p1_div[p1_ang>0][half//2:])+half//2, ax4)
 
     legend = plt.legend()
     plt.setp(legend.get_texts(), color='grey')
