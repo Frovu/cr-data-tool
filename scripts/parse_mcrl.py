9e74f81812c3c2a31009987e59f4ce218c02b208
Frovu <sfrovis@gmail.com>
2022-07-14 15:33:21 +0300

    fix: parse mcrl

--- scripts/parse_mcrl.py
+++ scripts/parse_mcrl.py
@@ -6,16 +6,21 @@ pg_conn = psycopg2.connect(
     password = os.environ.get("MUON_DB_PASS"),
     host = os.environ.get("MUON_DB_HOST") )
 
-SRC = 'tmp/MCRL/V'
+SRC = 'tmp/MCRL'
 
 if __name__ == '__main__':
     pressure, cube = [], []
     for fname in sorted(os.listdir(SRC)):
         with open(f'{SRC}/{fname}') as file:
             lines = file.readlines()
+        lasttime = 0
         for line in lines[2:]:
             sp = line.split()
             time = datetime.strptime(sp[0]+'T'+sp[1], '%Y-%m-%dT%H:%M:%S+00')
+            if time == lasttime:
+                print('skip double line', time)
+                continue
+            lasttime = time
             pressure.append([time, float(sp[26])])
             cube.append([time, float(sp[32])])
         print(f'[{len(cube)}] < {fname}')
@@ -23,11 +28,11 @@ if __name__ == '__main__':
     with pg_conn.cursor() as cursor:
         q = '''INSERT INTO muon_counts_60m (time, source, channel)
         SELECT time, source, (SELECT id FROM muon_channels WHERE station_name = \'Moscow-CUBE\')
-        FROM (VALUES %s) s(time, source) ON CONFLICT DO NOTHING'''
+        FROM (VALUES %s) s(time, source) ON CONFLICT (time, channel) DO UPDATE SET source = EXCLUDED.source'''
         psycopg2.extras.execute_values(cursor, q, cube)
         q = '''INSERT INTO muon_conditions_60m (time, pressure, station)
         SELECT time, pressure, (SELECT id FROM muon_stations WHERE name = \'Moscow-CUBE\')
-        FROM (VALUES %s) s(time, pressure) ON CONFLICT DO NOTHING'''
+        FROM (VALUES %s) s(time, pressure) ON CONFLICT (time, station) DO UPDATE SET pressure = EXCLUDED.pressure'''
         psycopg2.extras.execute_values(cursor, q, pressure)
         pg_conn.commit()
     print('done')
