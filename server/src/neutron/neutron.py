cc602e4d254ec4b290b3824c0c63a24f7c9592c6
Frovu <sfrovis@gmail.com>
2023-07-07 19:02:17 +0300

    fix something

--- server/src/neutron/neutron.py
+++ server/src/neutron/neutron.py
@@ -2,6 +2,7 @@ from database import pool, upsert_many
 from math import floor, ceil
 from threading import Lock
 from datetime import datetime, timezone
+from dataclasses import dataclass
 import os, logging
 import numpy as np
 
@@ -10,24 +11,33 @@ from neutron.nmdb import obtain as obtain_from_nmdb
 
 log = logging.getLogger('crdt')
 
+NMDB_SINCE = datetime(2020, 1, 1).replace(tzinfo=timezone.utc).timestamp()
 HOUR = 3600
 obtain_mutex = Lock()
 integrity_full = [None, None]
 integrity_partial = [None, None]
+all_stations = []
+
+@dataclass
+class Station:
+	id: str
+	provides_1min: bool
+	prefer_nmdb: bool
 
 def _init():
+	global integrity_full, integrity_partial, all_stations
 	with open(os.path.join(os.path.dirname(__file__), './_init_db.sql')) as file:
 		init_text = file.read()
 	with pool.connection() as conn:
 		conn.execute(init_text)
-		stations = conn.execute('SELECT id, provides_1min FROM neutron.stations').fetchall()
-		for station, with_1min in stations:
-			conn.execute(f'ALTER TABLE neutron.result ADD COLUMN IF NOT EXISTS {station} REAL')
-			conn.execute(f'CREATE TABLE IF NOT EXISTS nm.{station}_1h (time TIMESTAMPTZ PRIMARY KEY, corrected REAL, revised REAL)')
-			if not with_1min: continue
-			conn.execute(f'CREATE TABLE IF NOT EXISTS nm.{station}_1min (time TIMESTAMPTZ PRIMARY KEY, corrected REAL)')
+		rows = conn.execute('SELECT id, provides_1min, prefer_nmdb FROM neutron.stations').fetchall()
+		all_stations = [Station(*r) for r in rows]
+		for s in all_stations:
+			conn.execute(f'ALTER TABLE neutron.result ADD COLUMN IF NOT EXISTS {s.id} REAL')
+			conn.execute(f'CREATE TABLE IF NOT EXISTS nm.{s.id}_1h (time TIMESTAMPTZ PRIMARY KEY, corrected REAL, revised REAL)')
+			if not s.provides_1min: continue
+			conn.execute(f'CREATE TABLE IF NOT EXISTS nm.{s.id}_1min (time TIMESTAMPTZ PRIMARY KEY, corrected REAL)')
 		ff, ft, pf, pt = conn.execute('SELECT full_from, full_to, partial_from, partial_to FROM neutron.integrity_state').fetchone()
-		global integrity_full, integrity_partial
 		integrity_full, integrity_partial = [ff, ft], [pf, pt]
 _init()
 
@@ -38,9 +48,14 @@ def _save_integrity_state(conn):
 # filter spikes > 5 sigma
 # filter station periods with < 50% coverage
 def filter_for_integration(data):
+	print(data)
 	data[data <= 0] = np.nan
 	std = np.nanstd(data, axis=0)
 	med = np.nanmean(data, axis=0)
+	print(np.where(np.abs(med - data) / std > 5))
+	print()
+	print(med, std)
+	print()
 	data[np.where(np.abs(med - data) / std > 5)] = np.nan
 	data[:,np.where(np.count_nonzero(np.isfinite(data), axis=0) < len(data) / 2)] = np.nan
 	return data
@@ -49,8 +64,9 @@ def integrate(data):
 	data = filter_for_integration(data)
 	return np.round(np.sum(data, axis=0) / len(data), 3)
 
-def _obtain_similar(interval, stations, source, src_res=60):
-	src_data = { 'nmdb': obtain_from_nmdb, 'archive': obtain_from_archive }[source](interval, stations)
+def _obtain_similar(interval, stations, source):
+	obtain_fn, src_res = { 'nmdb': (obtain_from_nmdb, 60), 'archive': (obtain_from_archive, 3600) }[source]
+	src_data = obtain_fn(interval, stations)
 	if not src_data:
 		log.warn(f'Empty obtain ({source})!')
 		return # TODO: handle smh
@@ -68,9 +84,10 @@ def _obtain_similar(interval, stations, source, src_res=60):
 		data[:,0] = [datetime.utcfromtimestamp(t) for t in range(first_full_h, last_full_h+1, HOUR)]
 		step = floor(HOUR / src_res)
 		offset = floor((first_full_h - r_start) / src_res)
-		fdata = src_data[:,1:].astype(float)
+		fdata = np.vstack(src_data[:,1:].astype(float))
 		integrated = (integrate(fdata[offset+i*step:offset+(i+1)*step]) for i in range(length))
 		res = np.fromiter(integrated, np.dtype((float, len(stations))))
+		# for i in range(len(stations))
 		data[:,1:] = res
 	else:
 		data = src_data
@@ -85,17 +102,31 @@ def _obtain_similar(interval, stations, source, src_res=60):
 					np.column_stack((src_data[:,0], src_data[:,1+i])))
 			else:
 				assert src_res == HOUR
-		# TODO: insert corrected into result cach table
-		# upsert_many(conn, 'neutron.result', ['time', *stations], data)
+		# TODO: insert revisions into result cache table
+		upsert_many(conn, 'neutron.result', ['time', *stations], data)
 		conn.execute('INSERT INTO neutron.obtain_log(stations, source, interval_start, interval_end) ' +\
 			'VALUES (%s, %s, %s, %s)', [stations, source, *res_dt_interval])
 
 def _obtain_group(interval, group_partial=False):
+	# TODO: another criteria
+	stations = [s for s in all_stations if not group_partial or s.prefer_nmdb]
+	
+	assert group_partial
 
-	stations = ['APTY', 'CALG'] # TODO: determine stations by group
+	if interval[0] < NMDB_SINCE and NMDB_SINCE <= interval[1]:
+		_obtain_group((interval[0], NMDB_SINCE-HOUR), group_partial)
+		_obtain_group((NMDB_SINCE, interval[1]), group_partial)
+		return log.debug('Neutron: split interval with NMDB_SINCE')
 
-	# source = 'nmdb' if interval[1] >= datetime(datetime.now().year, 1, 1).timestamp() else 'local'
-	_obtain_similar(interval, stations, 'nmdb')
+	nmdb_stations = [s.id for s in stations if s.prefer_nmdb] if interval[0] >= NMDB_SINCE else []
+	nmdb_stations = ['KERG']
+	if nmdb_stations:
+		_obtain_similar(interval, nmdb_stations, 'nmdb')
+	
+	other_stations = [s.id for s in stations if s.id not in nmdb_stations]
+	if not other_stations: return
+	for s in other_stations:
+		_obtain_similar(interval, [s], 'archive')
 
 def fetch(interval: [int, int], stations: list[str]):
 	interval = (
--- server/src/routers/neutron.py
+++ server/src/routers/neutron.py
@@ -9,7 +9,7 @@ bp = Blueprint('neutron', __name__, url_prefix='/api/neutron')
 def get_neutron():
 	from datetime import datetime, timezone
 	neutron._obtain_group([
+		datetime(2023, 7, 6, 12).replace(tzinfo=timezone.utc).timestamp(),
 		datetime(2023, 7, 7, 0).replace(tzinfo=timezone.utc).timestamp(),
-		datetime(2023, 7, 7, 3).replace(tzinfo=timezone.utc).timestamp(),
-	])
+	], True)
 	return 'Hello World'
